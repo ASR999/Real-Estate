916ff6126a4fe569107d85b3746b336f4d8442cc
Ayush <ayushrawat00112@gmail.com>
2026-04-18 17:24:15 +0530

    connected to database

--- api/index.js
+++ api/index.js
@@ -1,8 +1,20 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((err) => {
+    console.log(`Error: ${err}`);
+  });
 
 const app = express();
 
-app.listen(3000, ()=> {
-     console.log('server is running on 3000')
-})
-
+app.listen(3000, () => {
+  console.log("server is running on 3000");
+});
